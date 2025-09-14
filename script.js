2a47fdca8879e0745f8147927651813380e8a9a3
Sanjib Rana <ranasanjib967@email.com>
2025-09-14 21:44:42 +0545

    final commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const apiKey = "08724a0d635366bb9a3323b65a1b2000"
+
+const weatherDataEle = document.querySelector(".weather-data")
+const cityNameEle = document.querySelector("#city-name")
+const formEle = document.querySelector("form")
+const imgIcon = document.querySelector(".icon")
+
+formEle.addEventListener("submit",(e)=>{
+    // console.log(cityNameEle.value)
+    e.preventDefault()
+    cityValue = cityNameEle.value
+
+    getWeatherData(cityValue)
+})
+
+async function getWeatherData(cityValue){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric
+`)
+        if(!response.ok){
+            throw new Error("Network response is not Ok!")
+        }
+
+        const data = await response.json()
+        console.log(data)
+        const tempreture = Math.floor(data.main.temp)
+        const description = data.weather[0].description
+        const icon = data.weather[0].icon
+
+        const details = [
+             `Feels Like: ${Math.floor(data.main.feels_like)}°C`,
+             `Humidity: ${data.main.humidity}%`,
+             `Wind Speed: ${data.wind.speed}m/s`
+        ]
+        
+        weatherDataEle.querySelector(".temp").textContent = `${tempreture + "°C"}`;
+        weatherDataEle.querySelector(".desc").textContent = `${description}`;
+        imgIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}.png" alt="">`
+
+        weatherDataEle.querySelector(".details").innerHTML = details.map((detail)=>`<div>${detail}</div>`).join("");
+
+    }catch(err){
+       weatherDataEle.querySelector(".temp").textContent = "";
+       imgIcon.innerHTML =""
+       weatherDataEle.querySelector(".desc").textContent = "An Error Occur"
+
+
+    }
+}
